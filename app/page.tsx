3b60de2daeaaaf7e9e4e86c34cf84888b751c442
opeyemibioku <opeyemibioku@gmail.com>
2025-03-02 01:21:49 +0100

    updated other option

--- app/page.tsx
+++ app/page.tsx
@@ -64,14 +64,26 @@ export default function SignInPage() {
   return (
     <>
       <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
-        <Image
+        {/* <Image
           src="/logo.png"
           alt="Company Logo"
           width={80}
           height={50}
           priority
-        />
-        <h2 className="text-2xl font-semibold mb-4 text-center">Sign In</h2>
+        /> */}
+        <div className="flex justify-center items-center mb-5">
+          <Image
+            src="/logo.png"
+            alt="Company Logo"
+            width={80}
+            height={50}
+            priority
+          />
+        </div>
+
+        <h2 className="text-2xl font-semibold mb-4 text-center mt-5">
+          Sign In
+        </h2>
 
         {/* Email */}
         <label className="block mb-2 font-medium">Email:</label>
