2c96c5b229b01ff24dafa47b99c7bdb2c58c4f85
opeyemibioku <opeyemibioku@gmail.com>
2025-03-10 14:30:16 +0100

    resolved duplicating table header when fetching responses

--- app/all-responses/page.tsx
+++ app/all-responses/page.tsx
@@ -237,7 +237,7 @@ const SurveyResponsesTable: React.FC = () => {
         }
         .table th,
         .table td {
-          min-width: 16rem;
+          min-width: 18rem;
           font-size: 0.7rem;
           word-wrap: break-word;
         }
